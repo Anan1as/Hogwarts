96b864bdb12f045ba41e917beb932c4d71941b2d
Ananias <amychelleoropezaortiz@gmail.com>
2023-12-04 17:20:55 -0500

    Iniciamos a armar las ideas

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+let iniciar = document.getElementById("iniciar");
+
+iniciar.addEventListener("click", function() {
+    let paso1 = document.getElementById("paso1");
+
+    let parrafo1 = document.createElement("p");
+    parrafo1.innerHTML = "¡Primero!, veamos como te describes a ti mismo y tu linaje.";
+    paso1.appendChild(parrafo1);
+    parrafo1.setAttribute("id", "parrafo1")
+
+    let 
+})
